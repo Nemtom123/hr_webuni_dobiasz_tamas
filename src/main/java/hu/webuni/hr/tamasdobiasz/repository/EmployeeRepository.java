3d6575d5e95a7d133d895f163a4aa461fc29feeb
Tomi <dobiasz.tamas@gmail.com>
2021-05-01 21:07:12 +0200

    6 hét javított változat

--- src/main/java/hu/webuni/hr/tamasdobiasz/repository/EmployeeRepository.java
+++ src/main/java/hu/webuni/hr/tamasdobiasz/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package hu.webuni.hr.tamasdobiasz.repository;
 
 import hu.webuni.hr.tamasdobiasz.model.Company;
+import org.springframework.data.domain.Page;
 import hu.webuni.hr.tamasdobiasz.model.Employee;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,25 +15,22 @@ import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    List<Employee> findBySalaryGreaterThan(Integer minSalary, Pageable pageable);
-    List<Employee> findByTitle(String title);
-    List<Employee> findByNameStartingWithIgnoreCase(String name);
-    List<Employee> findByEntryDateBetween(LocalDateTime start, LocalDateTime end);
-    @Modifying
-    @Transactional
-//	@Query("UPDATE Employee e "
-//			+ "SET e.salary = :minSalary "
-//			+ "WHERE e.position.name=:positionName "
-//			+ "AND e.salary< :minSalary "
-//			+ "AND e.company.id=:companyId")
-    @Query("UPDATE Employee e "
-            + "SET e.salary = :minSalary "
-            + "WHERE e.id IN ("
-            + "SELECT e2.id "
-            + "FROM Employee e2 "
-            + "WHERE e2.position.name=:positionName "
-            + "AND e2.salary< :minSalary "
-            + "AND e2.company.id=:companyId)")
-    int updateSalaries(String positionName, int minSalary, long companyId);
+	Page<Employee> findBySalaryGreaterThan(Integer minSalary, Pageable pageable);
+
+	List<Employee> findByPositionName(String title);
+	List<Employee> findByNameStartingWithIgnoreCase(String name);
+	List<Employee> findByEntryDateBetween(LocalDateTime start, LocalDateTime end);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE Employee e "
+			+ "SET e.salary = :minSalary "
+			+ "WHERE e.id IN ("
+			+ "SELECT e2.id "
+			+ "FROM Employee e2 "
+			+ "WHERE e2.position.name=:positionName "
+			+ "AND e2.salary< :minSalary "
+			+ "AND e2.company.id=:companyId)")
+	int updateSalaries(String positionName, int minSalary, long companyId);
 
 }
